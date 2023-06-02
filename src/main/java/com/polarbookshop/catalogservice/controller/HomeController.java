0203c9803ef08941367fba2d587af2c36691e453
rajanjha786 <rajanjha786786@gmail.com>
2023-06-02 21:28:02 +0530

    catalog-service initial commit

---
+++ src/main/java/com/polarbookshop/catalogservice/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.catalogservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String getGreeting() {
+        return "Welcome to the book catalog 2.0";
+    }
+}
